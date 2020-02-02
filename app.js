ddd89d6366789a6a61659a511cca01d24d96c5a9
moh-arriza-n <arriza@linuxmail.org>
2020-02-02 21:42:13 +0700

    test-1 file javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+http.createServer(function(req,res){
+res.writeHead(200,{
+"COntent-Type": "text/html"
+});
+res.end("Hello World !");
+}).listen.(8000);
+console.log('server berjalan pada port 8000');
